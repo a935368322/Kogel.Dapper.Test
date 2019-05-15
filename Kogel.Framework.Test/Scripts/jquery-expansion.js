13edb850f2dc4dc83c94ae5b98d287539ef2ce0a
935368322 <935368322@qq.com>
2019-05-15 10:15:35 +0800

    sss

--- Kogel.Framework.Test/Scripts/jquery-expansion.js
+++ Kogel.Framework.Test/Scripts/jquery-expansion.js
@@ -1,12 +1,17 @@
-﻿/*jQuery扩展方法*/
-var jQueryExpansion = {};
-//多事件绑定(需要配合adonis.full.js一起使用)
+﻿var jQueryExpansion = {};//事件容器
 $.fn.addBind = function (obj, callback) {
-    //绑定标识
-    var bindId = Common.getGuid().replace(/-/g, "");
-    if (jQueryExpansion[bindId] == undefined) {
+    //生成一个guid作为标识
+    var bindId = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+    //是否绑定过事件
+    if ($(this).attr("bind-id") == undefined) {
         jQueryExpansion[bindId] = [];
+        //绑定标识
         $(this).attr("bind-id", bindId);
+    } else {
+        bindId = $(this).attr("bind-id");
     }
     jQueryExpansion[bindId].push(callback);
     $(this).unbind();
@@ -15,8 +20,9 @@ $.fn.addBind = function (obj, callback) {
         for (var i = 0; i < jQueryExpansion[bindId].length; i++) {
             jQueryExpansion[bindId][i].call(this, e);
         }
-    });
+    })
 };
+
 //根据model写入form值
 $.fn.setForm = function (jsonValue, callback) {
     var obj = this;
@@ -55,6 +61,14 @@ $.fn.setForm = function (jsonValue, callback) {
         if (typeof callback === "function")
             callback.call($oinput != undefined && $oinput.length >= 1 ? $oinput[0] : undefined, name, ival);
     }
+}
+//jquery对象转数组
+$.fn.toArray = function (jsonValue) {
+    var array = [];
+    $(this).each(function (index, value) {
+        array.push(value);
+    });
+    return array;
 }
 //生成动态表条件
 $.fn.serializeQuery = function () {
